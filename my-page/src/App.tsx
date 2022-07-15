45422a6e8384853532168c8e66ffd71e4ccd7cf7
Andrii <badekha@gmail.com>
2022-07-15 20:51:16 +0300

    HW-4

--- my-page/src/App.tsx
+++ my-page/src/App.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import './App.css';
 import data from './data.json';
 import { Data } from './Data';
+import { FooterComponent } from './FooterComponent';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <Data data={data.firstName} />
-      <Data data={data.email} />
-      <Data data={data.phone} />
-    </header>
-  </div>
-);
+const App = () => {
+  const callback = (url: string) => {
+    console.log(`redirecting user to the next ${url}`)
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Data data={data.firstName} />
+        <Data data={data.email} />
+        <Data data={data.phone} />
+      </header>
+      <footer>
+        <FooterComponent
+          text={'lorem...'}
+          url={'https://www.google.com/'}
+          callback={callback}
+        />
+      </footer>
+    </div>
+  );
+};
 
 export default App;
